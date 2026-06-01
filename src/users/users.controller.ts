46ca6d2a7046987065ae81f5b2d8d3b6f01a0965
Karol Jędrzejak <karol.jedrzejak@gmail.com>
2026-06-01 14:43:34 +0200

    first commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,50 @@
+import { 
+    Controller,
+    Post,
+    Body,
+    Get,
+    Patch,
+    Param,
+    Query,
+    Delete,
+    NotFoundException
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UsersService } from './users.service';
+
+@Controller('auth')
+export class UsersController {
+    constructor(private usersService: UsersService) {}
+
+    @Post('/signup')
+    createUser(@Body() body: CreateUserDto) {
+        this.usersService.create(body.email, body.password);
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string) {
+        return this.usersService.find(email);
+    }
+
+    @Get('/:id')
+    findUser(@Param('id') id: string) {
+        const user = this.usersService.findOne(parseInt(id));
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
+        return user;
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+        return this.usersService.update(parseInt(id), body);
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        return this.usersService.remove(parseInt(id));
+    }
+
+
+}
