133e22100c2d97ecc9c9a59705015200e7580f54
Karol Jędrzejak <karol.jedrzejak@gmail.com>
2026-06-04 19:33:36 +0200

    make interceptors 2

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -7,13 +7,25 @@ import {
  } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { plainToClass } from 'class-transformer';
+import { plainToInstance } from 'class-transformer';
 
+export function Serialize(dto: any) {
+    return UseInterceptors(new SerializeInterceptor(dto));
+}
 
-@UseInterceptors(ClassSerializerInterceptor)
 export class SerializeInterceptor implements NestInterceptor {
+    constructor(private dto: any) {}
+
     intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-        return next.handle();
+        //console.log('Before handler...',context);
+        return next.handle().pipe(
+            map((data: any) => {
+                // console.log('Before sending response...',data);
+                return plainToInstance(this.dto, data, {
+                    excludeExtraneousValues: true,
+                });
+            }),
+        )  ;
     }
 }
 
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,11 +9,12 @@ import {
     Delete,
     NotFoundException,
     UseInterceptors,
-    ClassSerializerInterceptor
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -29,9 +30,10 @@ export class UsersController {
         return this.usersService.find(email);
     }
 
-    @UseInterceptors(ClassSerializerInterceptor)
+    @Serialize(UserDto)
     @Get('/:id')
     findUser(@Param('id') id: string) {
+        //console.log('Handler is running...');
         const user = this.usersService.findOne(parseInt(id));
         if (!user) {
             throw new NotFoundException('User not found');
