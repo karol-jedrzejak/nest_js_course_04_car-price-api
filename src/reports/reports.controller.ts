a65a867f3be6cb715eba304c04911f53431e55e2
Karol Jędrzejak <karol.jedrzejak@gmail.com>
2026-06-12 09:50:28 +0200

    add dtos for reports

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { 
+    Controller,
+    Post,
+    Get,
+    Body,
+    UseGuards
+} from '@nestjs/common';
+import { CreateReportDTO } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from '../guards/auth.guard';
+
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+    constructor(private reportService: ReportsService){
+
+    }
+
+    @Post()
+    @UseGuards(AuthGuard)
+    createReport(@Body() body:CreateReportDTO){
+        return this.reportService.create(body);
+    }
+
+}
